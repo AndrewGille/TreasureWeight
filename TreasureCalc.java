cbbae374e354cbbd458ed509fabb9a579ca017cd
AndrewGille <andy.gille@mwcradio.com>
2022-08-09 09:58:05 -0500

    Add files via upload
    Aug 9 - Added Array List to replace inelegant Case statements; expanded comments

--- TreasureCalc.java
+++ TreasureCalc.java
@@ -1,82 +1,88 @@
+import java.util.ArrayList;
 
 public class TreasureCalc {
 		
 	// Set Metal -----------------------------------
+	// Uses an Array List to determine metal name based on user input
+	// Version 2.0 will pull this from an updatable mysql db
 	
-	public String setMetal(String metal) {
+	public String setMetal(int metal) {
 		
-		switch (metal) {
-		case "1":
-			metal="gold";
-			break;
-		case "2":
-			metal="silver";
-			break;
-		case "3":
-			metal="copper";
-			break;
-		case "4":
-			metal="staters";
-			break;
-		case "5":
-			metal="ducats";
-			break;
-		case "6":
-			metal="decims";
-			break;
+		//subtract 1 from choice since Array LIst starts at (0), this will be more elegant in 2.0
+		metal=metal-1;
 		
-		}
-		return metal;
+		ArrayList<String> metals = new ArrayList<String>();
+	    metals.add("gold");
+	    metals.add("silver");
+	    metals.add("copper");
+	    metals.add("staters");
+	    metals.add("ducats");
+	    metals.add("decims");	    	    
+
+	    //Get metal name from Array List and set it to a returnable string 
+	    String setmetal = metals.get(metal);
+
+	    
+		return setmetal;
 	}
 	
 	
 	// Set Currency -----------------------------------
 	
-	public String setCurrency(String metal) {
+	public String setCurrency(int metal) {
 		
-		switch (metal) {
-		case "1":
-			metal="staters";
-			break;
-		case "2":
-			metal="ducats";
-			break;
-		case "3":
-			metal="decims";
-			break;
+		//subtract 1 from choice since Array LIst starts at (0)
+		metal=metal-1;
 		
-		}
-		return metal;
+		ArrayList<String> metals = new ArrayList<String>();
+	    metals.add("staters");
+	    metals.add("ducats");
+	    metals.add("decims");   
+	    
+	    //Get metal name from Array List and set it to a returnable string 
+	    
+	    String setmetal = metals.get(metal);
+		
+	
+		return setmetal;
 	}
 	
 	
 	// getWeight -----------------------------------
 
-	public double getWeight(String metal, double amt) {
+	public double getWeight(String metals, double amt) {
 		
+		//pulls in name of metal and weight of metal 
+		
+		
+		// declare weight variable
 		double weight=0.0;
 	
-		switch (metal) {
+		
+		//calculate metal weight by name 
+		switch (metals) {
+		
+		// Gold has a set value of 2000 Staters/oz
 		case "gold":
 			weight = amt*.0005;
 			weight = weight/16;
 		break;
-		
+		// Silver has a set value of 30 Staters/oz
 		case "silver":
 			weight = amt*.035;
 			weight = weight/16;
 		break;
-		
+		// Copper has a set value of .25 Staters/oz
 		case "copper":
 			weight = amt*4;
 			weight = weight/16;
 		break;
-		//staters roughly the weight of the scaajawea dollar
+		//staters roughly the weight of the US Sacagawea dollar coin
 		case "staters":
 			weight = amt*.26;
 			weight = weight/16;
 		break;
-		//ducats roughly the weight of a quarter
+		//ducats roughly the weight of a US quarter
 		case "ducats":
 			weight = amt*.2;
 			weight = weight/16;
@@ -89,18 +95,19 @@ public class TreasureCalc {
 	
 	// CoinCount -----------------------------------
 	
-	public double getCoinCount(String metal, double amt) {
+	public double getCoinCount(String metals, double amt) {
 		double coincount=0.0;
 		
-		switch (metal) {
-			//staters roughly the weight of the Sacajawea dollar
+		switch (metals) {
+		//staters roughly the weight of the Sacajawea dollar about 61/lb.
 		case "staters":
 			coincount = amt*61.53;		
 		break;
-		//ducats roughly the weight of a quarter
+		//ducats roughly the weight of a quarter about 80/lb.
 		case "ducats":
 			coincount = amt*80;
 		break;
+		// Decims are 1/10 oz of gold about the size of a penny and are worth about 200 Staters. 10/oz 160/lb.    
 		case"decims":
 			coincount = amt*160;
 		break;
--- TreasureWeight.java
+++ TreasureWeight.java
@@ -2,21 +2,30 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner; 
 
-
+/*
+ * ********************************************************************
+ * A program to calculate the weight of Treasure in the world of Kiron
+ * Helps develop currency system and understand how much treasure
+ * characters in the world need to haul!  
+ * 
+ * Version 2.0 with mysql connectivity and exchange rates coming soon!
+ * ********************************************************************
+ * 
+ */
 
 
 public class TreasureWeight {
 	
 
 	public static void main(String[] args)  {
-			
+	
 		//creates new instance of TreasureWeight class
 		TreasureWeight treasureweight = new TreasureWeight();
 		
 	
 
 	System.out.println("-----Treasure Weight----");
-		treasureweight.run();
+		treasureweight.run(); //Runs Program
 			
 
 	}
@@ -24,13 +33,11 @@ public class TreasureWeight {
 	// Run -----------------------------------
 
 	public void run() {
-		
+	
 		double weight =0.0;
 		double coincount = 0.0;
 		boolean again=true;
-		
-		
-		
+			
 			//Scanner
 			Scanner scanner = new Scanner(System.in);
 			
@@ -45,35 +52,54 @@ public class TreasureWeight {
 			int choice=scanner.nextInt();
 			
 				
-			//get Metal from user set Metal in TreasureCalc
+			//get Metal from user set Metal name in TreasureCalc
 			if (choice==1) {
 			
 				System.out.println("Enter Currency 1. Staters 2. Ducats 3. Decims:");
-				String metal = scanner.next();
-				metal = treasurecalc.setCurrency(metal);
+				int metal = scanner.nextInt();
+				
+				//Create metals string for name of metal 
+				
+				String metals;
+				metals = treasurecalc.setCurrency(metal);
+				
+				//User enters weight of the coins
+				
 				System.out.println("Enter amount in lbs:");
 				double amt = scanner.nextDouble();
 				
-				coincount = treasurecalc.getCoinCount(metal, amt);
-				System.out.println("Number of "+metal+" coins is "+coincount);
+				//Calculates number of coins for weight
+				
+				coincount = treasurecalc.getCoinCount(metals, amt);
+				System.out.println("Number of "+metals+" coins is "+coincount);
 				
 			
 			}else {
 				
+				//User enters type of currency to weigh
+				
 				System.out.println("Enter Currency 1. Gold, 2. Silver 3. Copper 4. Staters 5. Ducats :");
-				String metal = scanner.next();
-				metal = treasurecalc.setMetal(metal);
+				int metal = scanner.nextInt();
+				
+				//Create metals string for name of metal 
+				String metals;
+				metals = treasurecalc.setMetal(metal);
 			
+				//Enter the amount of coins expressed in reserve currency (Staters)
+				
 				System.out.println("Enter amount in staters:");
 				double amt = scanner.nextDouble();
 			
-				// staters to weight calculations
+				// Calculate weight based on total value of coins in reserve currency (Staters)
 		
-				weight = treasurecalc.getWeight(metal, amt);
-				// Output Weight of treausre in lbs
-				System.out.println("Weight of "+metal+" is "+weight+"lbs.");
+				weight = treasurecalc.getWeight(metals, amt);
+				
+				// Output Weight of coins in lbs
+				System.out.println("Weight of "+metals+" is "+weight+"lbs.");
 			
 			}
+			
+			//  Loop if user wants to perform another calculation
 			
 			System.out.println("Again? 1-yes 2-no");
 			
